935e30a5ad76e0c93418bb41087fc8889dfaf795
MedGm <elgorrim.mohamed@etu.uae.ac.ma>
2026-01-14 11:21:55 +0100

    controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -30,6 +30,7 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     try {
+        console.log('Login attempt:', req.body);
         const { email, password } = req.body;
         const user = await prisma.user.findUnique({ where: { email } });
 
